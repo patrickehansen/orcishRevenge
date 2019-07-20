11f6f5897bfd638cec10abd8e1e415e94e6eb8af
Tiskahar <patrick.e.hansen@gmail.com>
2019-07-19 22:54:11 -0500

    added the board, canvas and game components

---
+++ client/src/components/game/board.js
@@ -0,0 +1,49 @@
+'use strict';
+import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Canvas from './canvas';
+import ErrorComponent from '../util/error';
+import store from '../../store/store';
+import {setToken} from '../../store/actions/actions';
+import config from '../../../config';
+
+class Board extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        error: null,
+        loggedOut: false,
+      }
+    }
+
+    logout=(e) => {
+      e.preventDefault();
+
+      localStorage.removeItem(config.localstorageKey);
+      this.store.dispatch(setToken(null));
+
+      this.setState({
+        loggedOut: true,
+      })
+    }
+
+    render() {
+      if (this.state.loggedOut) {
+        return <Redirect to='/' />
+      }
+
+      return (
+        <Container component='div' className='board-root'>
+          <button className='logoutBtn' onClick={this.logout}>
+          Logout
+          </button>
+          <Canvas />
+          <ErrorComponent error={this.state.error} />
+        </Container>
+      )
+    }
+}
+
+export default Board;
---
+++ client/src/components/game/game.js
@@ -0,0 +1,50 @@
+'use strict';
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import SplitPane from 'react-split-pane';
+
+import Board from './board';
+import ChatRoll from './chatroll';
+import ErrorComponent from '../util/error';
+import SocketClient from '../../socket/socketClient';
+
+class Game extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        error: null,
+      }
+    }
+
+    componentDidMount() {
+      console.log('Game component mounted. connecting to socket..')
+      this.socketClient = new SocketClient({});
+    }
+
+    onSendChat = (message) => {
+      this.socketClient.SendChatMessage(message);
+    }
+
+    render() {
+      return (
+        <Container component='div' className='gameView-root'>
+          <SplitPane 
+            split='vertical' 
+            minSize={220} 
+            defaultSize={300} 
+            maxSize={650} 
+            primary='second'
+            >
+            <Board />
+            <ChatRoll 
+              onSendChat={this.onSendChat}
+            />
+          </SplitPane>
+          <ErrorComponent error={this.state.error} />
+        </Container>
+      )
+    }
+}
+
+export default Game;
