c8354633d6381e237a42b3d85639427e05113c0b
Tiskahar <patrick.e.hansen@gmail.com>
2019-11-18 17:06:12 -0600

    separating the client from the server repository

--- client/config.js
+++
@@ -1,7 +0,0 @@
-import ms from 'ms';
-
-export default {
-  server: process.env.NODE_ENV === 'development' ? 'http://localhost:6190' : 'https://orcish-revenge.herokuapp.com',
-  localstorageKey: 'PEHLLC-orcishRevenge-id_token',
-  errorClearTime: ms('5s'),
-}
